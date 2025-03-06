c63fa7075a7430719be7c5be7362e62a9298a767
Corentin Le Tortorec <cle-tort@paul-f5Ar3s10.clusters.42paris.fr>
2025-03-06 15:16:14 +0100

    mariadb and nginx images done

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-service mysql start
+#docker run --env-file ~/inception/srcs/.env -it mariadb
 
+/etc/init.d/mariadb start
+
+sleep 5
+
+echo $SQL_ROOT_PASSWORD
+echo $SQL_DATABASE
+echo $SQL_USER
+echo $SQL_PASSWORD
+
+
+mysql -e "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
-
-mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
+mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
+/etc/init.d/mariadb stop
+exec mysqld_safe
