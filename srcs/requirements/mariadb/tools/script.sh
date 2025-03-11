3c713c8c3823c551dcac46c375fad75a24d9fc43
Corentin Le Tortorec <cle-tort@paul-f4Ar5s9.clusters.42paris.fr>
2025-03-11 18:31:44 +0100

    save

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -6,17 +6,11 @@
 
 sleep 5
 
-echo $SQL_ROOT_PASSWORD
-echo $SQL_DATABASE
-echo $SQL_USER
-echo $SQL_PASSWORD
-
-
 mysql -e "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
-mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 /etc/init.d/mariadb stop
 exec mysqld_safe
