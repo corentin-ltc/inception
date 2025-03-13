1755a4add8a701ee0bd372d4888d8d6a18a7fd7e
corentin-ltc <153531202+corentin-ltc@users.noreply.github.com>
2025-03-14 00:19:15 +0100

    Update auto_config.sh

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+
+# Check if WordPress is already installed
 if [ ! -e /var/www/wordpress/index.php ]
 then
+    # Download WordPress if not already installed
     wp core download --allow-root --path=/var/www/wordpress
 
+    # Set permissions
     chown -R www-data:www-data /var/www/wordpress/*
     chmod -R 755 /var/www/wordpress/*
 
     # Generate wp-config.php
     wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb --allow-root --path=/var/www/wordpress
 
-    # Create site and users
+    # Install WordPress
     wp core install --url=https://cle-tort.42.fr --title="$WEBSITE_NAME" --admin_user="$ADMIN_NAME" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_EMAIL" --allow-root --path=/var/www/wordpress
 
-    wp user create "$USER_NAME" "$USER_EMAIL" --user_pass="$USER_PASSWORD" --allow-root --path=/var/www/wordpress"
+    # Create a user
+    wp user create "$USER_NAME" "$USER_EMAIL" --user_pass="$USER_PASSWORD" --allow-root --path=/var/www/wordpress
 fi
+
+# Execute the command passed as argument to the entrypoint
+exec "$@"
