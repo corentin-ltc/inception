5216b2115046e8fcaf47ab180ee14989ef01bc43
Corentin Le Tortorec <cle-tort@bess-f2r4s16.clusters.42paris.fr>
2025-03-13 23:03:23 +0100

    save

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -4,16 +4,6 @@ if [ ! -e /var/www/html/index.php ]
 then	
 	wp core download --allow-root
 
-	#WP-CONFIG
-	sed -i "s/database_name_here/$SQL_DATABASE/g" /var/www/html/wp-config-sample.php
-	sed -i "s/username_here/$SQL_USER/g" /var/www/html/wp-config-sample.php
-	sed -i "s/password_here/$SQL_PASSWORD/g" /var/www/html/wp-config-sample.php
-	sed -i "s/localhost/mariadb/g" /var/www/html/wp-config-sample.php
-	#echo "define('CONCATENATE_SCRIPTS', false);" >> /var/www/html/wp-config-sample.php
-	mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-
-	chown -R www-data:www-data /var/www/html/*
-	chmod -R 755 /var/www/html/*
 
 	#CREATE SITE + USERS
 	wp core install --url=https://cle-tort.42.fr --title=$WEBSITE_NAME --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root
