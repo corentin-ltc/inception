04f43db4fecdbe89a5da4add13e1fd8b38bb15b6
corentin-ltc <153531202+corentin-ltc@users.noreply.github.com>
2025-03-14 00:12:36 +0100

    Update auto_config.sh

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
+if [ ! -e /var/www/wordpress/index.php ]
+then
+    wp core download --allow-root --path=/var/www/wordpress
 
-if [ ! -e /var/www/html/index.php ]
-then	
-	wp core download --allow-root
+    chown -R www-data:www-data /var/www/wordpress/*
+    chmod -R 755 /var/www/wordpress/*
 
-	chown -R www-data:www-data /var/www/html/*
-	chmod -R 755 /var/www/html/*
+    # Generate wp-config.php
+    wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb --allow-root --path=/var/www/wordpress
 
-	#CREATE SITE + USERS
-	wp core install --url=https://cle-tort.42.fr --title=$WEBSITE_NAME --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root
+    # Create site and users
+    wp core install --url=https://cle-tort.42.fr --title="$WEBSITE_NAME" --admin_user="$ADMIN_NAME" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_EMAIL" --allow-root --path=/var/www/wordpress
 
-	wp user create $USER_NAME $USER_EMAIL --user_pass=$USER_PASSWORD --allow-root
+    wp user create "$USER_NAME" "$USER_EMAIL" --user_pass="$USER_PASSWORD" --allow-root --path=/var/www/wordpress"
 fi
-
-exec "$@"
